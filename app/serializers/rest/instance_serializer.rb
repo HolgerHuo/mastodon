50e61619a7d770c6813d08632793a8b7a2a5c1c7
Holger Huo <Holger Huo>
2021-03-13 19:18:32 +0800

    feat: make characters limit configurable

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -5,9 +5,13 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   attributes :uri, :title, :short_description, :description, :email,
              :version, :urls, :stats, :thumbnail,
-             :languages, :registrations, :approval_required, :invites_enabled
+             :languages, :registrations, :approval_required, :max_toot_chars, :invites_enabled
 
   has_one :contact_account, serializer: REST::AccountSerializer
+
+  def max_toot_chars
+    StatusLengthValidator::MAX_CHARS
+  end
 
   has_many :rules, serializer: REST::RuleSerializer
 
